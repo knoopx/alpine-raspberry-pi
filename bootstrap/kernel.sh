25577d3efc0208b79dce97a38d6da0532160292b
Victor Martinez <knoopx@gmail.com>
2020-09-29 19:36:43 +0200

    use alpine RPI kernel and firmware

--- bootstrap/kernel.sh
+++ bootstrap/kernel.sh
@@ -2,11 +2,25 @@
 
 set -xe
 
-echo "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait" > /boot/cmdline.txt
+echo "modules=loop,squashfs,sd-mod,usb-storage root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes console=tty1 rootwait quiet" > /boot/cmdline.txt
 
 cat <<EOF > /boot/config.txt
-disable_splash=1
-boot_delay=0
+[pi3]
+kernel=vmlinuz-rpi
+initramfs initramfs-rpi
+[pi3+]
+kernel=vmlinuz-rpi
+initramfs initramfs-rpi
+[pi4]
+enable_gic=1
+kernel=vmlinuz-rpi4
+initramfs initramfs-rpi4
+[all]
+arm_64bit=1
+include usercfg.txt
+EOF
+  
+cat <<EOF > /boot/usercfg.txt
 EOF
 
 # fstab
@@ -14,3 +28,6 @@ cat <<EOF > /etc/fstab
 /dev/mmcblk0p1  /boot           vfat    defaults          0       2
 /dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
 EOF
+
+apk add linux-rpi linux-rpi4 raspberrypi-bootloader
+cd /boot/dtbs-rpi && find -type f \( -name "*.dtb" -o -name "*.dtbo" \) | cpio -pudm /boot
