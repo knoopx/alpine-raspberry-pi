f5d186e55d6915fa29331a34df97d7476f82850f
Victor Martinez <knoopx@gmail.com>
2021-07-01 23:59:28 +0200

    parametrize user, pass and timezone

--- bootstrap/system.sh
+++ bootstrap/system.sh
@@ -3,19 +3,21 @@
 set -xe
 
 TARGET_HOSTNAME="raspberrypi"
+TARGET_TIMEZONE="Europe/Madrid"
+ROOT_PASS=raspberry
 
 # base stuff
 apk add ca-certificates
 update-ca-certificates
-echo "root:raspberry" | chpasswd
+echo "root:$ROOT_PASS" | chpasswd
 setup-hostname $TARGET_HOSTNAME
 echo "127.0.0.1    $TARGET_HOSTNAME $TARGET_HOSTNAME.localdomain" > /etc/hosts
 setup-keymap es es
 
 # time
 apk add chrony tzdata
-setup-timezone -z Europe/Madrid
+setup-timezone -z $TARGET_TIMEZONE
 
 # other stuff
-apk add nano htop curl wget bash bash-completion
+apk add nano htop curl wget bash bash-completion findutils
 sed -i 's/\/bin\/ash/\/bin\/bash/g' /etc/passwd
--- bootstrap/users.sh
+++ bootstrap/users.sh
@@ -3,6 +3,7 @@
 set -xe
 
 FIRST_USER_NAME=pi
+FIRST_USER_PASS=raspberry
 
 apk add sudo
 
@@ -10,11 +11,11 @@ for GRP in spi i2c gpio; do
 	addgroup --system $GRP
 done
 
-adduser -s /bin/bash -D $FIRST_USER_NAME
+adduser -s /bin/ash -D $FIRST_USER_NAME
 
 for GRP in adm dialout cdrom audio users video games input gpio spi i2c netdev; do
   adduser $FIRST_USER_NAME $GRP
 done
 
-echo "pi:raspberry" | /usr/sbin/chpasswd
-echo "pi ALL=NOPASSWD: ALL" >> /etc/sudoers
+echo "$FIRST_USER_NAME:$FIRST_USER_PASS" | /usr/sbin/chpasswd
+echo "$FIRST_USER_NAME ALL=NOPASSWD: ALL" >> /etc/sudoers
